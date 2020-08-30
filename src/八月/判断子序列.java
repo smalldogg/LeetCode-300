512df5dfcb286292871ee6aeccb2e5ee50076543
wangyinhui <13037651897@163.com>
2020-08-30 10:01:24 +0800

    补卡8月

---
+++ src/八月/判断子序列.java
@@ -0,0 +1,28 @@
+package 八月;
+
+/**
+ * @author wangyh
+ * @create 2020-08-29 22:49
+ */
+
+public class 判断子序列 {
+    /**
+     * 判断字符串s是否为t的子序列
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        if (s.length() > t.length()) return false;
+        int idx = 0;
+        char[] chs = s.toCharArray();
+        char[] cht = t.toCharArray();
+        for (int i = 0; i < cht.length; ++i) {
+            if (idx == chs.length) return true;
+            if (chs[idx] == cht[i]) idx++;
+        }
+        return idx == chs.length;
+    }
+}
---
+++ src/八月/递增子序列.java
@@ -0,0 +1,40 @@
+package 八月;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author wangyh
+ * @create 2020-08-29 23:08
+ */
+
+public class 递增子序列 {
+    /**
+     * 找到所有的递增子序列
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        if (nums == null || nums.length == 0) return new ArrayList<>();
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(nums, 0, results, path);
+        return results;
+    }
+
+    private void dfs(int[] nums, int idx, List<List<Integer>> results, List<Integer> path) {
+        if (path.size() >= 2) results.add(new ArrayList<>(path));
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = idx; i < nums.length; ++i) {
+            if (path.size() > 0 && nums[i] <
+                    path.get(path.size() - 1) || set.contains(nums[i])) continue;
+            path.add(nums[i]);
+            set.add(nums[i]);
+            dfs(nums, i + 1, results, path);
+            path.remove(path.size() - 1);
+        }
+    }
+
+}
