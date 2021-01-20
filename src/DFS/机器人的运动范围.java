2c2facd9cb78dc604668d68bd89534d138ecaa3a
wangyinhui <13037651897@163.com>
2021-01-20 15:28:19 +0800

    集器人的运动范围

--- src/DFS/机器人的运动范围.java
+++ src/DFS/机器人的运动范围.java
@@ -8,15 +8,17 @@ package DFS;
 public class 机器人的运动范围 {
     public int movingCount(int m, int n, int k) {
         boolean[][] v = new boolean[m][n];
-        return dfs(0, 0, m, n, k, v);
+        return walk(0, 0, m, n, k, v);
     }
 
-    private int dfs(int x, int y, int m, int n, int k, boolean[][] v) {
+    private int walk(int x, int y, int m, int n, int k, boolean[][] v) {
         if (x < 0 || x >= m || y < 0 || y >= n ||
                 v[x][y] || (getSum(x) + getSum(y)) > k) return 0;
         v[x][y] = true;
-        return 1 + dfs(x + 1, y, m, n, k, v) + dfs(x - 1, y, m, n, k, v)
-                + dfs(x, y + 1, m, n, k, v) + dfs(x, y - 1, m, n, k, v);
+        return 1 + walk(x + 1, y, m, n, k, v)
+								 + walk(x - 1, y, m, n, k, v)
+                 + walk(x, y + 1, m, n, k, v)
+							   + walk(x, y - 1, m, n, k, v);
     }
 
     private int getSum(int num) {
