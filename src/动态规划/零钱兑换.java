50bf15baf4bbeca11d7dcc407660bcea71418e19
wangyinhui <13037651897@163.com>
2021-02-10 17:56:02 +0800

    代码风格调整

--- src/动态规划/零钱兑换.java
+++ src/动态规划/零钱兑换.java
@@ -11,15 +11,16 @@ package 动态规划;
  * dp[i] = math.min(dp[i - coins[j]] + 1, dp[i]);
  */
 public class 零钱兑换 {
-		public int coinChange(int[] coins, int amount) {
-				int[] dp = new int[amount + 1];
-				dp[0] = 0;
-				for (int i = 1; i <= amount; ++i) {
-						dp[i] = Integer.MAX_VALUE;
-						for (int j = 0; j < coins.length; ++j) {
-								if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-						}
-				}
-				return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
-		}
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; ++j) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
 }
