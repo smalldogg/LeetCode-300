87e9cb0061a071fd76e2394b3e23da18f81c1bef
wangyinhui <13037651897@163.com>
2021-02-17 09:51:01 +0800

    代码调整

--- src/BFS/SynchronizedTest.java
+++ src/BFS/SynchronizedTest.java
@@ -8,40 +8,41 @@ import java.util.Random;
  */
 class MyThread implements Runnable {
 
-		public int total;
+    public int total;
 
-		public int count;
+    public int count;
 
-		public MyThread( ) {
-				total = 100;
-				count = 0;
-		}
+    public MyThread() {
+        total = 100;
+        count = 0;
+    }
 
-		@Override public void run( ) {
-				synchronized( this ) {
-						if( total > 0 ) {
-								try {
-										Thread.sleep( new Random( ).nextInt( 1000 ) );
-								} catch( InterruptedException e ) {
-										e.printStackTrace( );
-								}
-								count++;
-								total--;
-								System.out.println( Thread.currentThread( ).getName( ) + "\t当前票号：" + count );
-						}
-				}
-		}
+    @Override
+    public void run() {
+        synchronized (this) {
+            if (total > 0) {
+                try {
+                    Thread.sleep(new Random().nextInt(1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+                total--;
+                System.out.println(Thread.currentThread().getName() + "\t当前票号：" + count);
+            }
+        }
+    }
 }
 
 
 public class SynchronizedTest {
-		public static void main( String[] args ) {
-				MyThread t = new MyThread( );
-				for( int i = 1; i <= 3; i++ ) {
-						new Thread( t, "售票点" + i ).start( );
-				}
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        for (int i = 1; i <= 3; i++) {
+            new Thread(t, "售票点" + i).start();
+        }
 
 
-		}
+    }
 
 }
--- src/BFS/Test.java
+++ src/BFS/Test.java
@@ -16,24 +16,24 @@ import java.util.function.Predicate;
 
 public class Test {
 
-		public static void main( String[] args ) throws UnsupportedEncodingException {
-				long l = LocalDateTime.now( ).toInstant( ZoneOffset.of( "+8" ) ).toEpochMilli( );
-				System.out.println( l );
-				String s = "1591946163926";
-				long l2 = System.currentTimeMillis( );
-				long l1 = Long.parseLong( s );
-				System.out.println( l1 );
-				long time = 1591946163926l;
-				String str = String.format( "chuangqi%d", ( time >> ( ( time % 5 ) + 3 ) ) + time % 99999 );
-				String encode;
-				try {
-						encode = URLEncoder.encode( str, StandardCharsets.UTF_8.displayName( ) );
-						//String result = Base64Utils.encodeToString(encode.getBytes(StandardCharsets.UTF_8));
-						System.out.println( encode );
-				} catch( UnsupportedEncodingException e ) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        long l = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+        System.out.println(l);
+        String s = "1591946163926";
+        long l2 = System.currentTimeMillis();
+        long l1 = Long.parseLong(s);
+        System.out.println(l1);
+        long time = 1591946163926l;
+        String str = String.format("chuangqi%d", (time >> ((time % 5) + 3)) + time % 99999);
+        String encode;
+        try {
+            encode = URLEncoder.encode(str, StandardCharsets.UTF_8.displayName());
+            //String result = Base64Utils.encodeToString(encode.getBytes(StandardCharsets.UTF_8));
+            System.out.println(encode);
+        } catch (UnsupportedEncodingException e) {
 
-				}
-		}
+        }
+    }
 
 
 }
