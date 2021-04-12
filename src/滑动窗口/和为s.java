5455989345496d8b08f8b1a25b7033fa4402892a
wangyinhui <13037651897@163.com>
2021-04-12 20:28:16 +0800

    最大子数组

---
+++ src/滑动窗口/和为s.java
@@ -0,0 +1,19 @@
+package 滑动窗口;
+
+public class 和为s {
+
+
+    public boolean finds(int[] nums, int s) {
+        if (nums == null || nums.length == 0) return false;
+        int max = nums[0];
+        int cur = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            cur += nums[i];
+            max = Math.max(cur, max);
+            cur = cur < 0 ? 0 : cur;
+        }
+        return cur == s;
+    }
+
+
+}
